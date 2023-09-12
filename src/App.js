11564f4f5a2d3914cd0d903590c045a44ef0c7d2
Ritik Bhasarkar <ritikbhasarkar2401@gmail.com>
2023-09-12 22:41:57 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -2,97 +2,110 @@ import "./App.css";
 import Header from "./Header";
 import Footer from "./Footer";
 import Employees from "./Employees";
-import { useState } from "react";
+import GroupTeamMembers from "./GroupTeamMembers";
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-  const [selectedTeam, setTeam] = useState("TeamB");
+  const [selectedTeam, setTeam] =
+    useState(JSON.parse(localStorage.getItem("TeamList"))) || "TeamB";
 
-  const [employees, setEmployees] = useState([
-    {
-      id: 1,
-      fullName: "Bob Jones",
-      designation: "JavaScript Developer",
-      gender: "male",
-      teamName: "TeamA",
-    },
-    {
-      id: 2,
-      fullName: "Jill Bailey",
-      designation: "Node Developer",
-      gender: "female",
-      teamName: "TeamA",
-    },
-    {
-      id: 3,
-      fullName: "Gail Shepherd",
-      designation: "Java Developer",
-      gender: "female",
-      teamName: "TeamA",
-    },
-    {
-      id: 4,
-      fullName: "Sam Reynolds",
-      designation: "React Developer",
-      gender: "male",
-      teamName: "TeamB",
-    },
-    {
-      id: 5,
-      fullName: "David Henry",
-      designation: "DotNet Developer",
-      gender: "male",
-      teamName: "TeamB",
-    },
-    {
-      id: 6,
-      fullName: "Sarah Blake",
-      designation: "SQL Server DBA",
-      gender: "female",
-      teamName: "TeamB",
-    },
-    {
-      id: 7,
-      fullName: "James Bennet",
-      designation: "Angular Developer",
-      gender: "male",
-      teamName: "TeamC",
-    },
-    {
-      id: 8,
-      fullName: "Jessica Faye",
-      designation: "API Developer",
-      gender: "female",
-      teamName: "TeamC",
-    },
-    {
-      id: 9,
-      fullName: "Lita Stone",
-      designation: "C++ Developer",
-      gender: "female",
-      teamName: "TeamC",
-    },
-    {
-      id: 10,
-      fullName: "Daniel Young",
-      designation: "Python Developer",
-      gender: "male",
-      teamName: "TeamD",
-    },
-    {
-      id: 11,
-      fullName: "Adrian Jacobs",
-      designation: "Vue Developer",
-      gender: "male",
-      teamName: "TeamD",
-    },
-    {
-      id: 12,
-      fullName: "Devin Monroe",
-      designation: "Graphic Designer",
-      gender: "male",
-      teamName: "TeamD",
-    },
-  ]);
+  const [employees, setEmployees] = useState(
+    JSON.parse(localStorage.getItem("employeesList")) || [
+      {
+        id: 1,
+        fullName: "Bob Jones",
+        designation: "JavaScript Developer",
+        gender: "male",
+        teamName: "TeamA",
+      },
+      {
+        id: 2,
+        fullName: "Jill Bailey",
+        designation: "Node Developer",
+        gender: "female",
+        teamName: "TeamA",
+      },
+      {
+        id: 3,
+        fullName: "Gail Shepherd",
+        designation: "Java Developer",
+        gender: "female",
+        teamName: "TeamA",
+      },
+      {
+        id: 4,
+        fullName: "Sam Reynolds",
+        designation: "React Developer",
+        gender: "male",
+        teamName: "TeamB",
+      },
+      {
+        id: 5,
+        fullName: "David Henry",
+        designation: "DotNet Developer",
+        gender: "male",
+        teamName: "TeamB",
+      },
+      {
+        id: 6,
+        fullName: "Sarah Blake",
+        designation: "SQL Server DBA",
+        gender: "female",
+        teamName: "TeamB",
+      },
+      {
+        id: 7,
+        fullName: "James Bennet",
+        designation: "Angular Developer",
+        gender: "male",
+        teamName: "TeamC",
+      },
+      {
+        id: 8,
+        fullName: "Jessica Faye",
+        designation: "API Developer",
+        gender: "female",
+        teamName: "TeamC",
+      },
+      {
+        id: 9,
+        fullName: "Lita Stone",
+        designation: "C++ Developer",
+        gender: "female",
+        teamName: "TeamC",
+      },
+      {
+        id: 10,
+        fullName: "Daniel Young",
+        designation: "Python Developer",
+        gender: "male",
+        teamName: "TeamD",
+      },
+      {
+        id: 11,
+        fullName: "Adrian Jacobs",
+        designation: "Vue Developer",
+        gender: "male",
+        teamName: "TeamD",
+      },
+      {
+        id: 12,
+        fullName: "Devin Monroe",
+        designation: "Graphic Designer",
+        gender: "male",
+        teamName: "TeamD",
+      },
+    ]
+  );
+
+  useEffect(() => {
+    localStorage.setItem("employeesList", JSON.stringify(employees));
+  }, [employees]);
+
+  useEffect(() => {
+    localStorage.setItem("TeamList", JSON.stringify(selectedTeam));
+  }, [selectedTeam]);
 
   function handleTeamSelectionChange(event) {
     setTeam(event.target.value);
@@ -113,17 +126,31 @@ function App() {
 
   return (
     <div className="App">
-      <Header 
-      selectedTeam={selectedTeam}
-      teamMembers ={employees.filter((employee)=>employee.teamName===selectedTeam).length}
-      />
-      <Employees
-        employees={employees}
-        handleEmployeeCardClick={handleEmployeeCardClick}
-        handleTeamSelectionChange={handleTeamSelectionChange}
-        selectedTeam={selectedTeam}
-      />
-      <Footer />
+      <Router>
+        <Header
+          selectedTeam={selectedTeam}
+          teamMembers={
+            employees.filter((employee) => employee.teamName === selectedTeam)
+              .length
+          }
+        />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Employees
+                employees={employees}
+                handleEmployeeCardClick={handleEmployeeCardClick}
+                handleTeamSelectionChange={handleTeamSelectionChange}
+                selectedTeam={selectedTeam}
+              />
+            }
+          ></Route>
+          <Route path="/groupedTeamMembers" element={<GroupTeamMembers/>}></Route>
+        </Routes>
+
+        <Footer />
+      </Router>
     </div>
   );
 }
---
+++ src/GroupTeamMembers.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GroupTeamMembers = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default GroupTeamMembers
